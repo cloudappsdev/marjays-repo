387c491d295b5078477344b8787faf128fe9b3fb
cloudappsdev <webguynow@yahoo.com>
2024-02-19 04:24:24 -0600

    spirit for your venue

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,28 @@
-import marjaysThumb from '../assets/images/MarJays_D0419u_thumb.jpeg'
+import marjaysThumb from "../assets/images/MarJays_D0419u_thumb.jpeg";
 
 function Home() {
   return (
     <>
-        <h1>The MarJays Music</h1>
-        <h2>The MarJays and Friends perform for you<br/>From Duo + Percussion, up to Full Band for line-up</h2>
-        <br/>
-        <img src={marjaysThumb} />
-        
+      <h1>The MarJays Music</h1>
+      <h2>
+        The MarJays and Friends perform for you
+        <br />
+        From Duo + Percussion, up to Full Band for line-up
+      </h2>
+      <br />
+      <img src={marjaysThumb} />
 
-        <h2>Playing roots, americana, folk, blues, rock, country, eclectic, reggae and originals</h2>
-        <h2>There's Nothing Better than Live Music</h2>
-        <h3>Set an uplifting mood and spirit of your venue or live event, with a Set-List styled for the occasion!</h3>
-
+      <h2>
+        Playing roots, americana, folk, blues, rock, country, eclectic, reggae
+        and originals
+      </h2>
+      <h2>There's Nothing Better than Live Music</h2>
+      <h3>
+        Set an uplifting mood and spirit for your venue or live event, with a
+        Set-List styled for the occasion!
+      </h3>
     </>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
