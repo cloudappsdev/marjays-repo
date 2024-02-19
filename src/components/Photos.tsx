230a5fc9f4940ea03e1f0886798528cf03579e2e
cloudappsdev <webguynow@yahoo.com>
2024-02-19 04:13:56 -0600

    minor changes for navbar and photos iframe for now

--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -1,8 +1,19 @@
+const styleDim = {
+  width: "100%",
+  minHeight: "800px",
+  height: "100%",
+};
 
 function Photos() {
   return (
-    <div>Photos</div>
-  )
+    <>
+      <div>Photos</div>
+      <iframe
+        style={styleDim}
+        src="https://mysrv.info/MarJays/OsTap/TheMarJays_Chicago.html"
+      />
+    </>
+  );
 }
 
-export default Photos
+export default Photos;
--- src/components/Shows.tsx
+++ src/components/Shows.tsx
@@ -1,22 +1,94 @@
-const liSimple = {
-  listStyle: 'none'
+import "./Shows.css";
+interface showHistory {
+  venue: string;
+  town: string;
+  type: string;
 }
+
+const showHistoryDat: showHistory[] = [
+  {
+    venue: "Galway Horse Farms",
+    town: "Long Grove",
+    type: "Event",
+  },
+  {
+    venue: "LaGrange Mural and Arts Fest",
+    town: "LaGrange",
+    type: "Event",
+  },
+  {
+    venue: "Buckledown Brewery",
+    town: "Lyons",
+    type: "",
+  },
+  {
+    venue: "Stadium Club",
+    town: "Hodgkins",
+    type: "",
+  },
+  {
+    venue: "MadCats",
+    town: "Palatine",
+    type: "*",
+  },
+  {
+    venue: "Miss Kittys",
+    town: "Naperville",
+    type: "*",
+  },
+  {
+    venue: "Ballydoyles",
+    town: "Downers Grove",
+    type: "*",
+  },
+  {
+    venue: "On Tour Brewing",
+    town: "Chicago",
+    type: "*",
+  },
+];
+
+const custHdrLabels: string[] = ["Venue", "Location", "Type"];
 
 function Shows() {
   return (
     <>
-    <div>Shows</div>
-    <div>All our shows are here</div>
-    <ul style={liSimple}>
-      <li>Buckledown Brewery</li>
-      <li>Cubby Bear</li>
-      <li>Dive Bar</li>
-      <li>Os Tap</li>
-      <li>xyz</li>
-    </ul>
-    
+      <h2>Shows</h2>
+      <p>
+        <div>
+          After a brief hiatus for the winter, we will be getting out playing -
+          actively booking and playing events as they come up
+        </div>
+        <div>
+          As you can see below - there are many areas - venues, we would
+          consider, so Help us Schedule something! We hope to see you soon
+        </div>
+      </p>
+      <br />
+      <br />
+      <table id="showHist" style={{ margin: "0 auto" }}>
+        <thead>
+          <tr>
+            {custHdrLabels.map((heading) => {
+              return <th key={heading}>{heading}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {showHistoryDat.map((row, index) => {
+            return (
+              <tr key={index}>
+                {Object.entries(row).map(([k, v], ndx: number) => {
+                  return <td key={ndx}>{v}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>* - indicates informal, impromtu appearance or open-mic</div>
     </>
-  )
+  );
 }
 
-export default Shows
+export default Shows;
