a4e29733a5ef4443befdac62a61cf3829fcba245
cloudappsdev <webguynow@yahoo.com>
2025-03-08 21:51:02 -0600

    remove extra files from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import "./App.css";
-import Bios from "./components/Bios";
-import Navbar from "./components/Navbar";
-import Shows from "./components/Shows";
-import Samplers from "./components/Samplers";
-import Videos from "./components/Videos";
-import Layout from "./components/Layout";
-import Photos from "./components/Photos";
-import Music from "./components/Music";
 
 function App() {
   return <Home></Home>;
